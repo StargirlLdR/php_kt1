08504975db8b4bad0359a9d6ad91b76d2c23e6c0
StargirlLdR <151396019+StargirlLdR@users.noreply.github.com>
2025-01-30 19:30:48 +0300

    Create php_kt1.php

---
+++ php_kt1.php
@@ -0,0 +1,27 @@
+<?php
+$bye_count = 0;
+
+while (true) {
+    $year = mt_rand(1930, 1950);  
+    $input_text = readline('>');   
+  
+    if (substr($input_text, -1) === '!') {
+        if (trim($input_text) === 'ПОКА!') {
+            $bye_count++;
+          
+            if ($bye_count === 3) {
+                echo 'ДО СВИДАНИЯ, МИЛЫЙ!' . PHP_EOL;
+                break;
+            } else {
+                echo 'НЕТ, НИ РАЗУ С ' . $year . ' ГОДА!' . PHP_EOL;
+            }
+        } else {
+            echo 'НЕТ, НИ РАЗУ С ' . $year . ' ГОДА!' . PHP_EOL;
+            $bye_count = 0; 
+        }
+    } else {
+        echo 'АСЬ?! ГОВОРИ ГРОМЧЕ, ВНУЧЕК!' . PHP_EOL;
+        $bye_count = 0; 
+    }
+}
+?>
